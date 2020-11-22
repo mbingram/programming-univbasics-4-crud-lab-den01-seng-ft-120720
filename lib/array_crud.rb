f386b238b2d051989c8d4d09591689c6ff8731c9
charming-input-2976 <mbingramm@gmail.com>
2020-11-22 18:18:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [bread, nutella, tea, coffee]
+  ["bread", "nutella", "tea", "coffee"]
 end
 
 def add_element_to_end_of_array(array, element)
